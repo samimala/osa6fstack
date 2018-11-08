092c162207c2761de451fdceced295beb0cfa137
Sami Mäläskä <epsilon@welho.com>
2018-11-08 20:37:44 +0200

    Teht 4

---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,30 @@
+const notificationAtStart = 'Wellcome'
+
+const initialState = notificationAtStart
+
+const notificationReducer = (store = initialState, action) => {
+  switch(action.type) {
+  case 'SHOW':
+    return action.content 
+  case 'HIDE':
+    return '' 
+  }
+  return store
+}
+  
+export const notifyActionCreator = {
+  newNotification(content) {
+    console.log('NEW notification')
+    return { 
+      type: 'SHOW', 
+      content: content
+    }
+  },
+  emptyNotification() {
+    return {
+      type: 'HIDE'
+    }
+  }
+}
+
+export default notificationReducer
